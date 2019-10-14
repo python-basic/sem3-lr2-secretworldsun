626e803387c90e19c39991059b1c8fd3e53e6588
secretworldsun <54934609+secretworldsun@users.noreply.github.com>
2019-10-14 21:51:03 +0300

    Код на Python для формулы Герона

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
-def foo(a,b,c):
-     return 42
+"""
+Яхимович Анастасия 
+ИВТ 2 курс 
+Группа 1.1
+Задание: Вычисление площади треугольника по формуле Герона
+"""
+
+def geron_sq(a,b,c):
+    p = (a+b+c)/2
+    import math
+    s = math.sqrt((p-a)*(p-b)*(p-c)*p)
+    return s
+
+def main():
+    a = int(input("a="))
+    b = int(input("b="))
+    c = int(input("c="))
+    print(geron_sq(a,b,c))
+
+main()
